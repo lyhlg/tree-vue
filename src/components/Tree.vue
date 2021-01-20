66dcbd6f69a1ecb9e1ff3849eacec656a5004538
ian <ian.lee@healingpaper.com>
2021-01-20 10:52:16 +0900

    updated style

--- src/components/Tree.vue
+++ src/components/Tree.vue
@@ -2,8 +2,8 @@
   <div class="tree-browser">
     <!-- Tree의 마지막 노드일때 -->
     <div v-if="!hasChildren">
-      <div :style="{ margin: `0 ${depth * 10}px 0 ${depth * 10}px` }">
-        <label :for="node.label" :class="getClass">
+      <div :style="{ margin: `0 5px 0 5px` }">
+        <label :for="node.label" :class="getSelectedClass">
           <input
             type="checkbox"
             class="node"
@@ -16,33 +16,41 @@
         </label>
       </div>
     </div>
+
     <!-- Tree의 마지막 노드가 아닐때 -->
-    <div v-else :class="expanded ? 'expanded' : 'fold'">
-      <div :style="{ 'margin-left': `${depth * 20}px` }" @click="nodeClicked">
+    <div class="label-wrapper" v-else :class="expanded ? 'expanded' : 'fold'">
+      <div
+        class="item-wrapper"
+        :style="{ 'margin-left': `${depth * 20}px` }"
+        @click="nodeClicked"
+      >
         <div>
           <span class="type">
-            {{ expanded ? "&#9660;" : "&#9658;" }}
+            {{ expanded ? "&#8897;" : ">" }}
           </span>
           <span>{{ node.label }}</span>
         </div>
       </div>
 
-      <div
-        :class="!child.children ? 'leaf-node' : ''"
-        v-for="child in node.children"
-        :key="child.name"
-      >
-        <Tree
-          v-if="expanded"
-          :node="child"
-          :depth="depth + 1"
-          :selected="selected"
-          @change="node => $emit('change', node)"
-        />
+      <div :class="getFinalNode" :style="{ 'margin-left': `${depth * 20}px` }">
+        <div
+          :class="!child.children ? 'leaf-node' : ''"
+          v-for="child in node.children"
+          :key="child.name"
+        >
+          <Tree
+            v-if="expanded"
+            :node="child"
+            :depth="depth + 1"
+            :selected="selected"
+            @change="node => $emit('change', node)"
+          />
+        </div>
       </div>
     </div>
   </div>
 </template>
+// :class="!node.children ? 'leaf-node-wrapper' : ''"
 
 <script>
 export default {
@@ -74,8 +82,14 @@ export default {
     }
   },
   computed: {
-    getClass() {
+    getSelectedClass() {
       return this.selected.includes(this.node.key) ? "selected" : "unselected";
+    },
+    getFinalNode() {
+      const finalNodeGroup =
+        this.node.children.findIndex(item => item.children) < 0;
+
+      return finalNodeGroup ? "leaf-node-final-group" : "";
     },
     hasChildren() {
       const { children } = this.node;
@@ -109,7 +123,25 @@ export default {
   text-align: left;
   font-size: 18px;
 }
+
 .expanded > .leaf-node {
   display: inline-flex;
+}
+
+.expanded > .leaf-node:hover {
+  font-weight: bold;
+}
+
+/* .expanded >  */
+.leaf-node-final-group {
+  display: flex;
+}
+
+.label-wrapper {
+  margin: 20px 0;
+}
+
+.item-wrapper {
+  margin: 10px 0;
 }
 </style>
